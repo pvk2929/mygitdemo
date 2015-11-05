5bf7ee4bc2987ee52028759232abab97ea7edf40
=Vijay <=pvk2929@gmail.com>
2015-11-05 17:53:17 +0530

    initial commit

---
+++ src/test/java/sample/git/SimpleGitTest.java
@@ -0,0 +1,30 @@
+package sample.git;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class SimpleGitTest {
+
+	@Test
+	public void testGit(){
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.flipkart.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		SoftAssert sa = new SoftAssert();
+		String title = driver.getTitle();
+		System.out.println("title is "+title);
+		sa.assertTrue(title.contains("Flipkart"), " Title doesnot contains Flipkart");
+		driver.close();
+		driver.quit();
+		sa.assertAll();
+		
+	}
+	
+}
